9358bec4017c48f4af224470575725efe2e7837d
Vikas Phulriya <88418134+vikasphulariya@users.noreply.github.com>
2023-06-25 17:17:46 +0530

    Update solidtyBegginer.sol

--- solidtyBegginer.sol
+++ solidtyBegginer.sol
@@ -4,8 +4,8 @@ pragma solidity 0.8.18;
 contract MyToken {
 
     // public variables here
-    string public tokenName="Meta";
-    string public tokenAbbrv="MTA";
+    string public tokenName="Vikas_Phulriya";
+    string public tokenAbbrv="VP";
     uint public totalSupply=0;
     // mapping variable here
     mapping (address=>uint) public balances;
